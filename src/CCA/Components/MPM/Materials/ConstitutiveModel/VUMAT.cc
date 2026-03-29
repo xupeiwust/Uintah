aaf436ed4469813d238abb3f0e49f9c9ffc1e26f
James Guilkey <james.guilkey@utah.edu>
2026-03-28 18:03:13 -0600

    In the previous commit, forgot to add the new files, VUMAT.h/cc

---
+++ src/CCA/Components/MPM/Materials/ConstitutiveModel/VUMAT.cc
@@ -0,0 +1,379 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 1997-2026 The University of Utah
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <CCA/Components/MPM/Materials/ConstitutiveModel/VUMAT.h>
+#include <Core/Grid/Patch.h>
+#include <CCA/Ports/DataWarehouse.h>
+#include <Core/Grid/Variables/ParticleVariable.h>
+#include <CCA/Components/MPM/Core/MPMLabel.h>
+#include <Core/ProblemSpec/ProblemSpec.h>
+#include <Core/Grid/Level.h>
+#include <Core/Math/MinMax.h>
+#include <Core/Math/Matrix3.h>
+#include <CCA/Components/MPM/Materials/MPMMaterial.h>
+#include <Core/Grid/Variables/VarTypes.h>
+#include <Core/Malloc/Allocator.h>
+//#include "svkVUMAT.cpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <dlfcn.h>
+
+using namespace std;
+using namespace Uintah;
+
+// Material Constants are C1, C2 and PR (poisson's ratio).  
+// The shear modulus = 2(C1 + C2).
+
+VUMAT::VUMAT(ProblemSpecP& ps, MPMFlags* Mflag) 
+  : ConstitutiveModel(Mflag)
+{
+  ps->getWithDefault("LIBRARY",d_initialData.library,"junk");
+  ps->getWithDefault("FUNCTION",d_initialData.function,"junk");
+  ps->getWithDefault("NSTATEV",d_initialData.nstatev,0);
+  ps->getWithDefault("VUMAT1",d_initialData.V1,0.0);
+  ps->getWithDefault("VUMAT2",d_initialData.V2,0.0);
+}
+
+VUMAT::~VUMAT()
+{
+}
+
+// Load the VUMAT function
+int VUMAT::loadLibrary(const char* libraryFile,
+              const char* functionName) {
+
+    // Load the library containing the VUMAT function
+    // NEED RTLD_GLOBAL to use VUAMT's library dependencies
+    lib_handle = dlopen(libraryFile, RTLD_LAZY | RTLD_GLOBAL);
+
+    if (!lib_handle) {
+      std::cout << "Failed to open library" << std::endl;
+      return 1;
+    }
+
+    // Get handle to function
+    void *func_handle = dlsym(lib_handle,functionName);
+
+    if (func_handle == NULL) {
+      std::cout << "Failed to load function" << std::endl;
+      return 1;
+    }
+
+    vumat_func = reinterpret_cast<vumat_handle>(func_handle);
+
+    return 0;
+}
+
+void VUMAT::outputProblemSpec(ProblemSpecP& ps,bool output_cm_tag)
+{
+  ProblemSpecP cm_ps = ps;
+  if (output_cm_tag) {
+    cm_ps = ps->appendChild("constitutive_model");
+    cm_ps->setAttribute("type","VUMAT");
+  }
+    
+  cm_ps->appendElement("LIBRARY",d_initialData.library);
+  cm_ps->appendElement("FUNCTION",d_initialData.function);
+  cm_ps->appendElement("NSTATEV",d_initialData.nstatev);
+  cm_ps->appendElement("VUMAT1",d_initialData.V1);
+  cm_ps->appendElement("VUMAT2",d_initialData.V2);
+}
+
+VUMAT* VUMAT::clone()
+{
+  return scinew VUMAT(*this);
+}
+
+void 
+VUMAT::initializeCMData(const Patch* patch,
+                                   const MPMMaterial* matl,
+                                   DataWarehouse* new_dw)
+{
+  // Initialize the variables shared by all constitutive models
+  // This method is defined in the ConstitutiveModel base class.
+  initSharedDataForExplicit(patch, matl, new_dw);
+
+  computeStableTimeStep(patch, matl, new_dw);
+}
+
+void VUMAT::computeStableTimeStep(const Patch* patch,
+                                             const MPMMaterial* matl,
+                                             DataWarehouse* new_dw)
+{
+  // This is only called for the initial timestep - all other timesteps
+  // are computed as a side-effect of computeStressTensor
+  Vector dx = patch->dCell();
+  int dwi = matl->getDWIndex();
+  // Retrieve the array of constitutive parameters
+  ParticleSubset* pset = new_dw->getParticleSubset(dwi, patch);
+  constParticleVariable<double> pmass, pvolume;
+  constParticleVariable<Vector> pvelocity;
+
+  new_dw->get(pmass,     lb->pMassLabel,     pset);
+  new_dw->get(pvolume,   lb->pVolumeLabel,   pset);
+  new_dw->get(pvelocity, lb->pVelocityLabel, pset);
+
+  double c_dil = 0.0;
+  Vector WaveSpeed(1.e-12,1.e-12,1.e-12);
+  double E  = d_initialData.V1;
+//  double PR = d_initialData.V2;
+
+  for(ParticleSubset::iterator iter = pset->begin();
+      iter != pset->end(); iter++){
+     particleIndex idx = *iter;
+
+     // Compute wave speed + particle velocity at each particle, 
+     // store the maximum
+     c_dil = sqrt(E*pvolume[idx]/pmass[idx]);
+     WaveSpeed=Vector(Max(c_dil+fabs(pvelocity[idx].x()),WaveSpeed.x()),
+                      Max(c_dil+fabs(pvelocity[idx].y()),WaveSpeed.y()),
+                      Max(c_dil+fabs(pvelocity[idx].z()),WaveSpeed.z()));
+  }
+  WaveSpeed = dx/WaveSpeed;
+  double delT_new = WaveSpeed.minComponent();
+  if(delT_new < 1.e-12)
+    new_dw->put(delt_vartype(DBL_MAX), lb->delTLabel, patch->getLevel());
+  else
+    new_dw->put(delt_vartype(delT_new), lb->delTLabel, patch->getLevel());
+}
+
+void VUMAT::computeStressTensor(const PatchSubset* patches,
+                                           const MPMMaterial* matl,
+                                           DataWarehouse* old_dw,
+                                           DataWarehouse* new_dw)
+{
+  for(int p=0;p<patches->size();p++){
+    const Patch* patch = patches->get(p);
+    Matrix3 Identity,B;
+    Identity.Identity();
+    double c_dil = 0.0,se=0.0;
+    Vector WaveSpeed(1.e-12,1.e-12,1.e-12);
+    Vector dx=patch->dCell();
+
+    int dwi = matl->getDWIndex();
+
+    // Create array for the particle position
+    ParticleSubset* pset = old_dw->getParticleSubset(dwi, patch);
+    constParticleVariable<Matrix3> pDeformGrad, pstressOld;
+    constParticleVariable<Matrix3> velGrad;
+    ParticleVariable<Matrix3> pstress;
+    constParticleVariable<double> pmass;
+    constParticleVariable<double> pvolume;
+    constParticleVariable<Vector> pvelocity;
+    ParticleVariable<double> pdTdt, p_q;
+
+    delt_vartype delT;
+    old_dw->get(delT, lb->delTLabel, getLevel(patches));
+
+    old_dw->get(pmass,               lb->pMassLabel,               pset);
+    old_dw->get(pvelocity,           lb->pVelocityLabel,           pset);
+    old_dw->get(pstressOld,          lb->pStressLabel,             pset);
+    new_dw->get(pvolume,             lb->pVolumeLabel_preReloc,    pset);
+    new_dw->get(pDeformGrad, lb->pDeformationMeasureLabel_preReloc,pset);
+    new_dw->get(velGrad,             lb->pVelGradLabel_preReloc,   pset);
+
+    new_dw->allocateAndPut(pstress,  lb->pStressLabel_preReloc,    pset);
+    new_dw->allocateAndPut(pdTdt,    lb->pdTdtLabel,               pset);
+    new_dw->allocateAndPut(p_q,      lb->p_qLabel_preReloc,        pset);
+
+    double E  = d_initialData.V1;
+    double PR = d_initialData.V2;
+
+    double props[] = {E, PR};
+    int nblock = 1, ndir = 3, nshr = 3, nstatev = 0, nprops = 2;
+    const int& iptr = 0;
+    const double& dptr = 0.0;
+    Matrix3 tensorU, tensorR;
+
+    // Load up the specific vumat library described by the arguments below
+    loadLibrary(d_initialData.library.c_str(), d_initialData.function.c_str());
+
+    for(ParticleSubset::iterator iter = pset->begin();iter!=pset->end();iter++){
+      particleIndex idx = *iter;
+
+      // This is evidently not used by this model
+      Matrix3 SO = pstressOld[idx];
+      double stressOld[6]={SO(0,0),SO(1,1),SO(2,2), SO(0,1), SO(1,2), SO(2,0)}; 
+
+      // Compute polar decomposition of F (F = RU)
+      pDeformGrad[idx].polarDecompositionRMB(tensorU, tensorR);
+
+      double stressNew[6] = {0.0};
+      double strainInc[6] = {0.0}; // Not used
+      double stretchNew[6] = {tensorU(0,0), tensorU(1,1), tensorU(2,2), 
+                              tensorU(0,1), tensorU(1,2), tensorU(0,2)};
+
+      // Call the VUMAT function directly
+      vumat_func(nblock, ndir, nshr, nstatev, iptr, nprops, iptr, 
+                 dptr, dptr, dptr, nullptr, nullptr, nullptr, 
+                 props, nullptr, strainInc, nullptr, nullptr, nullptr, 
+                 nullptr, nullptr, stressOld, nullptr, nullptr, nullptr, 
+                 nullptr, stretchNew, nullptr, nullptr, stressNew, nullptr, 
+                 nullptr, nullptr);
+
+      // Assign zero internal heating by default - modify if necessary.
+      pdTdt[idx] = 0.0;
+
+      pstress[idx] = Matrix3(stressNew[0], stressNew[3], stressNew[5],
+                             stressNew[3], stressNew[1], stressNew[4],
+                             stressNew[5], stressNew[4], stressNew[2]);
+
+      // Compute wave speed + particle velocity at each particle, 
+      // store the maximum
+      double rho_cur = pmass[idx]/pvolume[idx];
+      c_dil = sqrt(E/rho_cur);
+      WaveSpeed=Vector(Max(c_dil+fabs(pvelocity[idx].x()),WaveSpeed.x()),
+                       Max(c_dil+fabs(pvelocity[idx].y()),WaveSpeed.y()),
+                       Max(c_dil+fabs(pvelocity[idx].z()),WaveSpeed.z()));
+
+      // Compute artificial viscosity term
+      if (flag->d_artificial_viscosity) {
+        double dx_ave = (dx.x() + dx.y() + dx.z())/3.0;
+        double bulk = d_initialData.V1/(3.*(1. -2.*d_initialData.V2));
+        double c_bulk = sqrt(bulk/rho_cur);
+        Matrix3 D=(velGrad[idx] + velGrad[idx].Transpose())*0.5;
+        p_q[idx] = artificialBulkViscosity(D.Trace(), c_bulk, rho_cur, dx_ave);
+      } else {
+        p_q[idx] = 0.;
+      }
+
+      // Compute the strain energy for all the particles
+      double e = 0.;  // Fix this
+
+      se += e;
+    }  // end loop over particles
+
+    // Close down
+    dlclose(lib_handle);
+
+    WaveSpeed = dx/WaveSpeed;
+    double delT_new = WaveSpeed.minComponent();
+
+    if(delT_new < 1.e-12)
+      new_dw->put(delt_vartype(DBL_MAX), lb->delTLabel);
+    else
+      new_dw->put(delt_vartype(delT_new), lb->delTLabel, patch->getLevel());
+
+    if (flag->d_reductionVars->accStrainEnergy ||
+        flag->d_reductionVars->strainEnergy) {
+      new_dw->put(sum_vartype(se),      lb->StrainEnergyLabel);
+    }
+  }
+}
+
+void VUMAT::carryForward(const PatchSubset* patches,
+                                    const MPMMaterial* matl,
+                                    DataWarehouse* old_dw,
+                                    DataWarehouse* new_dw)
+{
+  for(int p=0;p<patches->size();p++){
+    const Patch* patch = patches->get(p);
+    int dwi = matl->getDWIndex();
+    ParticleSubset* pset = old_dw->getParticleSubset(dwi, patch);
+
+    // Carry forward the data common to all constitutive models 
+    // when using RigidMPM.
+    // This method is defined in the ConstitutiveModel base class.
+    carryForwardSharedData(pset, old_dw, new_dw, matl);
+
+    // Carry forward the data local to this constitutive model 
+    new_dw->put(delt_vartype(1.e10), lb->delTLabel, patch->getLevel());
+    
+    if (flag->d_reductionVars->accStrainEnergy ||
+        flag->d_reductionVars->strainEnergy) {
+      new_dw->put(sum_vartype(0.),     lb->StrainEnergyLabel);
+    }
+  }
+}
+
+         
+void VUMAT::addParticleState(std::vector<const VarLabel*>& from,
+                                        std::vector<const VarLabel*>& to)
+{
+}
+
+void VUMAT::addComputesAndRequires(Task* task,
+                                              const MPMMaterial* matl,
+                                              const PatchSet* patches ) const
+{
+  // Add the computes and requires that are common to all explicit 
+  // constitutive models.  The method is defined in the ConstitutiveModel
+  // base class.
+  const MaterialSubset* matlset = matl->thisMaterial();
+  addSharedCRForHypoExplicit(task, matlset, patches);
+}
+
+void 
+VUMAT::addComputesAndRequires(Task* ,
+                                   const MPMMaterial* ,
+                                   const PatchSet* ,
+                                   const bool ) const
+{
+}
+
+double VUMAT::computeRhoMicroCM(double pressure,
+                              const double p_ref,
+                              const MPMMaterial* matl,
+                              double temperature,
+                              double rho_guess)
+{
+  double rho_orig = matl->getInitialDensity();
+  double bulk = d_initialData.V1/(3.*(1. -2.*d_initialData.V2));
+
+  double p_gauge = pressure - p_ref;
+  double rho_cur;
+
+  rho_cur = rho_orig*(p_gauge/bulk + sqrt((p_gauge/bulk)*(p_gauge/bulk) +1));
+
+  return rho_cur;
+}
+
+void VUMAT::computePressEOSCM(double rho_cur,double& pressure,
+                                         double p_ref,
+                                         double& dp_drho, double& tmp,
+                                         const MPMMaterial* matl, 
+                                         double temperature)
+{
+  double bulk = d_initialData.V1/(3.*(1. -2.*d_initialData.V2));
+  double rho_orig = matl->getInitialDensity();
+  double shear = d_initialData.V1/(2.*(1+d_initialData.V2));
+
+  double p_g = .5*bulk*(rho_cur/rho_orig - rho_orig/rho_cur);
+  pressure = p_ref + p_g;
+  dp_drho  = .5*bulk*(rho_orig/(rho_cur*rho_cur) + 1./rho_orig);
+  tmp = (bulk + 4.*shear/3.)/rho_cur;  // speed of sound squared
+}
+
+double VUMAT::getCompressibility()
+{
+  double bulk = d_initialData.V1/(3.*(1. -2.*d_initialData.V2));
+  return 1.0/bulk;
+}
+
+namespace Uintah {
+} // End namespace Uintah
---
+++ src/CCA/Components/MPM/Materials/ConstitutiveModel/VUMAT.h
@@ -0,0 +1,175 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 1997-2026 The University of Utah
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef __VUMAT_H__
+#define __VUMAT_H__
+
+
+#include <CCA/Components/MPM/Materials/ConstitutiveModel/ConstitutiveModel.h>
+#include <Core/Math/Matrix3.h>
+#include <Core/ProblemSpec/ProblemSpecP.h>
+#include <CCA/Ports/DataWarehouseP.h>
+
+#include <cmath>
+#include <string>
+
+namespace Uintah {
+  class MPMLabel;
+  class MPMFlags;
+
+  /**************************************
+CLASS
+   VUMAT VUMAT
+   
+   Short description...
+
+GENERAL INFORMATION
+
+   VUMAT.h
+
+   Author: Jim Guilkey
+   Department of Mechanical Engineering
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+
+KEYWORDS
+   VUMAT VUMAT
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+  ****************************************/
+
+  class VUMAT : public ConstitutiveModel {
+    // Create datatype for storing model parameters
+  public:
+    struct CMData {
+      std::string function;
+      std::string library;
+      int nstatev;
+      double V1;
+      double V2;
+    };
+  private:
+    CMData d_initialData;
+         
+    // Prevent copying of this class
+    // copy constructor
+    //VUMAT(const VUMAT &cm);
+    VUMAT& operator=(const VUMAT &cm);
+
+  public:
+    // constructor
+    VUMAT(ProblemSpecP& ps, MPMFlags* flag);
+    //    VUMAT(const VUMAT* cm);
+         
+    // destructor 
+    virtual ~VUMAT();
+
+    virtual void outputProblemSpec(ProblemSpecP& ps,bool output_cm_tag = true);
+
+    //
+    // Define VUMAT API
+    typedef void (*vumat_handle)(
+  // Input arguments                             
+    const int &nblock, const int &ndir, const int &nshr, const int &nstatev,
+    const int &nfieldv, const int &nprops, const int &lanneal,
+    const double &stepTime, const double &totalTime, const double &dt,
+    const char *cmname, const double *coordMp, const double *charLength,
+    const double *props, const double *density, const double *strainInc,
+    const double *relSpinInc, const double *tempOld, const double *stretchOld,
+    const double *defgradOld, const double *fieldOld, const double *stressOld,
+    const double *stateOld, const double *enerInternOld,
+    const double *enerInelasOld, const double *tempNew,const double *stretchNew,
+    const double *defgradNew, const double *fieldNew,
+  // Output arguments                            
+    double *stressNew, double *stateNew, double *enerInternNew,
+    double *enerInelasNew );
+
+    // Putting library and function pointers in anonymous namespace for ease
+    void *lib_handle;
+    vumat_handle vumat_func;
+
+    // Load the VUMAT function
+    int
+    loadLibrary(const char* libraryFile,
+                const char* functionName);
+
+    // clone
+    VUMAT* clone();
+         
+    // compute stable timestep for this patch
+    virtual void computeStableTimeStep(const Patch* patch,
+                                       const MPMMaterial* matl,
+                                       DataWarehouse* new_dw);
+         
+    // compute stress at each particle in the patch
+    virtual void computeStressTensor(const PatchSubset* patches,
+                                     const MPMMaterial* matl,
+                                     DataWarehouse* old_dw,
+                                     DataWarehouse* new_dw);
+
+    // carry forward CM data for RigidMPM
+    virtual void carryForward(const PatchSubset* patches,
+                              const MPMMaterial* matl,
+                              DataWarehouse* old_dw,
+                              DataWarehouse* new_dw);
+
+    virtual double computeRhoMicroCM(double pressure,
+                                     const double p_ref,
+                                     const MPMMaterial* matl, 
+                                     double temperature,
+                                     double rho_guess);
+
+    virtual void computePressEOSCM(double rho_m, double& press_eos,
+                                   double p_ref,
+                                   double& dp_drho, double& ss_new,
+                                   const MPMMaterial* matl, 
+                                   double temperature);
+         
+    virtual double getCompressibility();
+
+    // initialize  each particle's constitutive model data
+    virtual void initializeCMData(const Patch* patch,
+                                  const MPMMaterial* matl,
+                                  DataWarehouse* new_dw);
+
+    virtual void addComputesAndRequires(Task* task,
+                                        const MPMMaterial* matl,
+                                        const PatchSet* patches) const;
+
+    virtual void addComputesAndRequires(Task* task,
+                                        const MPMMaterial* matl,
+                                        const PatchSet* patches,
+                                        const bool recursion) const;
+
+    virtual void addParticleState(std::vector<const VarLabel*>& from,
+                                  std::vector<const VarLabel*>& to);
+  };
+} // End namespace Uintah
+
+#endif  // __VUMAT_H__ 
